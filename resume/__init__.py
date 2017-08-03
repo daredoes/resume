f6fbe0f8fff3500affac37dd6d4bf332c39ca4b6
Daniel Evans <migetman9@gmail.com>
2017-08-03 15:59:45 -0700

    fix menu and form

--- resume/__init__.py
+++ resume/__init__.py
@@ -10,9 +10,9 @@ template_overrides(join(resume_config['module_root'], 'templates'))
 mount_site_sections(resume_config['module_root'])
 
 c.MENU['{{ c.CURRENT_ADMIN.first_name }} {{ c.CURRENT_ADMIN.last_name }}'].append_menu_item(
-    MenuItem(name='My Resume', href='../resume/?id={{ c.CURRENT_ADMIN.id }}')
+    MenuItem(name='My Resume', href='{{ c.PATH }}/resume/?id={{ c.CURRENT_ADMIN.id }}')
 )
 
 c.MENU['{{ c.CURRENT_ADMIN.first_name }} {{ c.CURRENT_ADMIN.last_name }}'].append_menu_item(
-    MenuItem(name='Edit Resume', href='../resume/edit?id={{ c.CURRENT_ADMIN.id }}')
+    MenuItem(name='Edit Resume', href='{{ c.PATH }}/resume/edit?id={{ c.CURRENT_ADMIN.id }}')
 )
